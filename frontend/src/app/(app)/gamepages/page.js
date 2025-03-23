383ab535a5aadb20fd0aef55d3eb51e109601ba8
ben-nguyen-22 <benjamin.t.nguyen02@gmail.com>
2025-03-23 17:52:57 +0900

    authentication is now working

---
+++ frontend/src/app/(app)/gamepages/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StartPage = () => {
+    return (
+        <div>
+            <Link href="gamepages/test">go to page 2</Link>
+        </div>
+    );
+};
+
+export default StartPage;
