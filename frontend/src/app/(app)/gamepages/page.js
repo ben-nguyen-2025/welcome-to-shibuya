d82c1ceb2748731b0b20d2cb3b91b30dcb97bf48
NSimon1121 <tup76485@temple.edu>
2025-03-31 10:19:35 +0900

    push and pull

--- frontend/src/app/(app)/gamepages/page.js
+++ frontend/src/app/(app)/gamepages/page.js
@@ -1,12 +1,28 @@
+'use client';
+import Header from '@/app/(app)/Header';
 import React from 'react';
 import Link from 'next/link';
 
 const StartPage = () => {
+    const [count, setCount] = React.useState(0);
+
     return (
-        <div>
-            <Link href="gamepages/test">go to page 2</Link>
-        </div>
+        <>
+            <Header title="PAGE 2" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg text-center">
+                        <Link
+                            href="/gamepages"
+                            className="absolute top-2/3 left-1/2 transform -translate-x-[100px] -translate-y-[70px]
+                                       bg-black text-white py-6 px-8 rounded-lg text-xl font-bold">
+                            Start the Game!
+                        </Link>
+                    </div>
+                    <img className="mx-auto" src="/shibuya-intro.jpg" />
+                </div>
+            </div>
+        </>
     );
 };
-
 export default StartPage;
