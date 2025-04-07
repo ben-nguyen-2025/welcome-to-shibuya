758d9e8694ac880786e222bd6cfa95be67da3095
NSimon1121 <tup76485@temple.edu>
2025-04-07 19:18:56 +0900

    adding imperial palace changes

--- frontend/src/app/(app)/gamepages/imperial_palace/page.js
+++ frontend/src/app/(app)/gamepages/imperial_palace/page.js
@@ -36,41 +36,73 @@ async function fetchUserPoints(userId) {
 
 const story = {
     start: {
-        text: "After a short walk north, something catches your eye-a massive torii gate, towering above you, it's dark wood standing in stark contrast to the bright city around it. Behind the gate, a lush forest seems to appear out of nowhere, its dense trees a surreal sight amidst the concrete jungle of Tokyo. To your left, a small sandwich shop hums with the quiet chatter of locals, and just beside it a sign welcomes visitors with a simple yet elegant message: “Welcome to Meiji Jingu”.",
+        text: 'Heading northeast from Meiji Jingu, you embark on quite a long trek, eventually coming face to face with what appears to be an important bridge. You cross over it, taking note of the wide moats that encircle the grounds of what seems to be a palace. Upon reaching the other side, the first thing that strikes you is the unexpected tranquility, especially for a palace in the heart of Tokyo. The lush greenery of the gardens and the tall trees create a serene atmosphere, offering a stark contrast to the city’s bustling energy that you’ve grown so accustomed to. It quickly becomes clear that you’ve arrived at the imperial palace.',
         choices: {
-            'Enter the torii gate': { next: 'torii_gate' },
-            'Stop for lunch': { next: 'lunch' },
+            'Keep going': { next: 'imperial_palace' },
         },
     },
 
-    start_food: {
-        text: "After a short walk north, something catches your eye-a massive torii gate, towering above you, it's dark wood standing in stark contrast to the bright city around it. Behind the gate, a lush forest seems to appear out of nowhere, its dense trees a surreal sight amidst the concrete jungle of Tokyo. To your left, a small sandwich shop hums with the quiet chatter of locals, and just beside it a sign welcomes visitors with a simple yet elegant message: “Welcome to Meiji Jingu”.",
+    imperial_palace: {
+        text: 'Entering the palace grounds, you are greeted by lush grass and meticulously maintained bushes and trees. People relaxing on benches that line the walkway, truly adding to the already peaceful atmosphere of the palace.',
         choices: {
-            'Enter the torii gate': { next: 'torii_gate' },
+            'Take a break': { next: 'take_break' },
+            Continue: { next: 'east_garden' },
         },
     },
 
-    lunch: {
-        text: 'In all your excitement exploring Shibuya you almost forgot to stop for some lunch, luckily not even fifty steps away is a quaint little shop known as “Mori no Terrace”. Stepping inside, you’re greeted by warm, wooden walls that echo the Meiji Jingu forest’s concept of “recycle”, giving the place a cozy, natural feel. The soft hum of conversation fills the air as you approach the menu, which boasts a tempting selection of sandwiches, coffee, and pastries.',
+    take_break: {
+        text: 'Finding the nearest bench, you take off your bag and sit down. As you observe the rolling clouds above, the sound of people chatting softly in the background becomes like white noise, almost lulling you into a peaceful sleep',
         choices: {
-            'Get a sandwich/drink ': {
-                next: 'get_food',
+            'Doze Off': { next: 'doze_off' },
+            'Get up and get going': { next: 'east_garden' },
+        },
+    },
+
+    doze_off: {
+        text: 'Closing your eyes for just a moment, you suddenly wake to a tap on your shoulder. It’s a worker, informing you that the palace is about to close and it’s time to leave. In your sleepy haze, you realize you’ve slept the day away by mistake.',
+        choices: {
+            restart: {
+                next: '',
+                effect: { movePage: 'shibuya_station', zeroPoints: true },
             },
         },
     },
 
-    get_food: {
-        text: 'After perusing the menu, you settle on an egg and tuna sandwich paired with a rich cappuccino. You take your food and find a spot by the window. As you take a bite, your gaze drifts outside, the bustling city of Shibuya sprawling in every direction. The sharp contrast between the urban energy and the serene, green expanse of Meiji Jingu’s forest just behind you strikes you - a perfect moment of calm amidst the chaos.',
+    east_garden: {
+        text: 'As you walk out of the front gardens, you approach what appears to be the inner grounds of the palace, only to find that it’s closed. Curious, you ask one of the guards, who simply shakes his head and informs you that the inner grounds are open to the public only twice a year. A bit disappointed, you turn toward the direction of the East Gardens.',
         choices: {
-            'Go Back': { next: 'start_food' },
+            Next: { next: 'next' },
         },
     },
 
-    torii_gate: {
-        text: 'Walking through the towering gates, you wander for what feels like an eternity, passing small exhibits, statues, and monuments dedicated to the gods. In the distance, a temple rises amidst lush green trees, its grandeur drawing crowds of people who queue eagerly to enter.',
+    next: {
+        text: 'As you’ve heard in the rumors, the gardens are enormous. It takes quite a while to get around, but as you walk through, you take note of the remnants of Edo Castle and discover a few more green spaces to explore. Not finding much else to do in this section, you decide to move on to the final area: Kitanomaru Park.',
         choices: {
-            'Enter the Temple': { next: 'enter_temple' },
-            'Walk Away': { next: 'walk_away' },
+            'Go to Park': { next: 'park' },
+        },
+    },
+
+    park: {
+        text: 'Walking through the park, you notice many attractions, from museums and arenas to boat rides. Fortunately, you arrived in spring, one of the best times to visit, with the flowers in full bloom.',
+        choices: {
+            'Take a boat ride': {
+                next: 'boat_ride',
+                effect: { addPoints: 10 },
+            },
+            'Leave the palace': {
+                next: '',
+                effect: { movePage: 'tokyo_tower' },
+            },
+        },
+    },
+
+    boat_ride: {
+        text: 'Right next to the park, in the moat section, you find a dock lined with boats available for rent. After chatting with an employee for a while, you choose a boat and head out onto the lake. Fortunately, it’s spring, and as you row through the moat, the cherry blossoms, in full bloom, dance in the wind, scattering petals around you. You spend some time just floating in the moment, until your time runs out and you’re forced to leave the palace.',
+        choices: {
+            'Leave the palace': {
+                next: '',
+                effect: { movePage: 'tokyo_tower' },
+            },
         },
     },
 
@@ -91,14 +123,8 @@ const story = {
     enter_temple_c: {
         text: 'Entering the temple, you are greeted by a sea of people. Some adding to the towering wall of ema near the old prayer tree, others watch the occasional wedding ceremony pass by, while a few at the front prepare for a ceremony of their own. At either end of the shrine stands two gates, labeled “East Gate” and “West Gate” respectively. ',
         choices: {
-            'Go east': {
-                next: '',
-                effect: { movePage: 'tokyo_tower' },
-            },
-            'Go northeast': {
-                next: '',
-                effect: { movePage: 'imperial_palace' },
-            },
+            'Go through east gate': { next: 'east_gate' },
+            'Go through west gate': { next: 'west_gate' },
         },
     },
 
@@ -112,33 +138,21 @@ const story = {
     east_gate: {
         text: 'You decide to pass through the East Gate. After walking about 100 paces, you enter a field filled with stalls selling an assortment of charms and memorabilia. At the far end of the stalls, a path leads you back to the front of Meiji Jingu.',
         choices: {
-            'Go east': {
-                next: '',
-                effect: { movePage: 'tokyo_tower' },
-            },
-            'Go northeast': {
-                next: '',
-                effect: { movePage: 'imperial_palace' },
-            },
+            'Go east': { next: 'tokyo_tower' },
+            'Go northeast': { next: 'imperial_palace' },
         },
     },
 
     west_gate: {
         text: 'Going through the West Gate, you find it leads to a side exit with a small animal petting zoo on the other side. Unfortunately, it seems there’s not much else here, you go back to the front of Meiji Jingu.',
         choices: {
-            'Go east': {
-                next: '',
-                effect: { movePage: 'tokyo_tower' },
-            },
-            'Go northeast': {
-                next: '',
-                effect: { movePage: 'imperial_palace' },
-            },
+            'Go east': { next: 'tokyo_tower' },
+            'Go northeast': { next: 'imperial_palace' },
         },
     },
 };
 
-function meiji_jingu() {
+function imperial_palace() {
     const router = useRouter();
     const [currentNode, setCurrentNode] = useState('start');
     const [inventory, setInventory] = useState([]);
@@ -164,11 +178,6 @@ function meiji_jingu() {
             setInventory(prev => [
                 ...new Set([...prev, nextNode.effect.addItem]),
             ]);
-        }
-
-        if (nextNode.effect?.movePage) {
-            router.push(nextNode.effect.movePage); // Navigate to the specified page
-            return;
         }
 
         if (nextNode.effect?.addPoints && userId) {
@@ -200,6 +209,11 @@ function meiji_jingu() {
                 console.log('Points reset successfully');
                 setPoints(0);
             }
+        }
+
+        if (nextNode.effect?.movePage) {
+            router.push(nextNode.effect.movePage); // Navigate to the specified page
+            return;
         }
 
         if (nextNode.next) {
@@ -248,5 +262,5 @@ function meiji_jingu() {
     );
 }
 
-export default meiji_jingu;
+export default imperial_palace;
 // export default introPage;
